16bf23953f46e1b7cc267432ce12434703ad725e
Matt Labrum <matt@labrum.me>
2011-05-31 15:29:41 +0930

    Fix main include file to load routes

--- URLRouter.php
+++ URLRouter.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once(dirname(__FILE__) . '/lib/Router.class.php');
+
 require_once(dirname(__FILE__) . '/lib/Route.class.php');
+require_once(dirname(__FILE__) . '/lib/StaticRoute.class.php');
+require_once(dirname(__FILE__) . '/lib/ConditionalRoute.class.php');
 
 ?>
