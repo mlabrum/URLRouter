ab65e9cb36ef75b961a22f74b6827c94140983bc
Matt Labrum <matt@labrum.me>
2011-05-31 15:30:25 +0930

    Fix URLRouter loading incorrect paths

--- URLRouter.php
+++ URLRouter.php
@@ -2,8 +2,8 @@
 
 require_once(dirname(__FILE__) . '/lib/Router.class.php');
 
-require_once(dirname(__FILE__) . '/lib/Route.class.php');
-require_once(dirname(__FILE__) . '/lib/StaticRoute.class.php');
-require_once(dirname(__FILE__) . '/lib/ConditionalRoute.class.php');
+require_once(dirname(__FILE__) . '/lib/routes/Route.class.php');
+require_once(dirname(__FILE__) . '/lib/routes/StaticRoute.class.php');
+require_once(dirname(__FILE__) . '/lib/routes/ConditionalRoute.class.php');
 
 ?>
